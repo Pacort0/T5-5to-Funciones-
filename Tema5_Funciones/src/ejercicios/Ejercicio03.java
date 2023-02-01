93ae4e888c2f2a5f0da03b269b364091cf0d47e2
frodriguez <frodriguez@106-24.info2>
2023-02-01 14:39:15 +0100

    inicio del 3

---
+++ Tema5_Funciones/src/ejercicios/Ejercicio03.java
@@ -0,0 +1,19 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio03 {
+
+	public static void main(String[] args) {
+		int longitud;
+		int limite;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca la longitud de la tabla: ");
+		longitud = sc.nextInt();
+		System.out.println("Introduzca el límite para los números de relleno: ");
+		limite = sc.nextInt();
+	}
+
+}
