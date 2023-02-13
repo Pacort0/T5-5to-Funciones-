3950980eb280f950c21528c761ee609d96a602d7
frodriguez <frodriguez@106-24.info2>
2023-02-13 09:36:23 +0100

    corregido error de suma ejercicio2

--- Tema5_Funciones/src/ejercicios/Ejercicio05.java
+++ Tema5_Funciones/src/ejercicios/Ejercicio05.java
@@ -7,19 +7,19 @@ public class Ejercicio05 {
 
 	public static void main(String[] args) {
 		int tam;
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Introduzca un tamaño para la tabla: ");
 		tam = sc.nextInt();
-		
+
 		int tabla[] = new int[tam];
-		
-		for(int i=0; i<tabla.length; i++) {
-			tabla[i] = (int)(Math.random()*10);
+
+		for (int i = 0; i < tabla.length; i++) {
+			tabla[i] = (int) (Math.random() * 10);
 		}
 		System.out.println(Arrays.toString(tabla));
-		
+
 		System.out.println("La tabla sin repetidos es: " + Arrays.toString(sinRepetidos(tabla)));
 		sc.close();
 	}
@@ -27,11 +27,13 @@ public class Ejercicio05 {
 	public static int[] sinRepetidos(int tabla[]) {
 		int tablaSR[] = new int[0];
 		int cont = 0;
-
-		for(int i=0; i<tabla.length; i++) {
-			
-		}
 		
+		do {
+			tablaSR = Arrays.copyOf(tablaSR, tablaSR.length+1);
+			tablaSR[tablaSR.length-1] = tabla[cont];
+			cont++;
+		}while(cont < tabla.length);
+
 		return tablaSR;
 	}
 
